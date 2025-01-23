6cec7d5f2a9034774e12a7592137e1c1e49d538f
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2025-01-22 23:12:50 -0500

    [test] handle None values

--- run.py
+++ run.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     # os.environ['Eaccount']   = 
     # os.environ['Epassword']  = 
 
-    SendByEmail(recipients = [os.getenv('ERECIPIENT')],
+    SendByEmail(recipients = [os.getenv('ERECIPIENT','jinsanity07_mTCrnk@kindle.com')],
                 subject=f'Eco{current_date}')
     if push == "mail":
         mail_push('https://zzzwb.us.kg/test')
